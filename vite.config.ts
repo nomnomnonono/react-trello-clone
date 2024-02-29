46b51f88deee6510c74159c63ee2160c77fc4ef2
nomnomnonono <murumuru1204@gmail.com>
2024-02-29 12:34:46 +0900

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    environment: "happy-dom",
+    setupFiles: ["./vitest-setup.ts"],
+    resolveSnapshotPath: (path, extension) => {
+      return path.replace("/src/", "/__snapshots__/") + extension;
+    },
+  },
+});
